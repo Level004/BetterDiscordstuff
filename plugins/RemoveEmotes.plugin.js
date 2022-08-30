389126ba3ebce1c7b24de839d209ffbe3cb1d038
Guilliano van Dinther <74723217+Level004@users.noreply.github.com>
2022-08-30 20:21:33 +0200

    working on adding multiple emotes

--- plugins/RemoveEmotes.plugin.js
+++ plugins/RemoveEmotes.plugin.js
@@ -2,53 +2,70 @@
  * @name Remove Emotes
  * @author Level 4
  * @description removes emotes from anywhere!
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 const chatBox = document.querySelector('[data-list-id="chat-messages"]');
+const blackLisedEmotes = ["thisguy", "unko"];
 
 function checkMessagesForEmotes() {
-    const selectedScroller = document.getElementById('app-mount');
-    const allMessages = selectedScroller.querySelectorAll(".messageListItem-ZZ7v6g");
-    const reactionSelect = selectedScroller.querySelectorAll(".reactionInner-YJjOtT");
-    for (const selectedMessage of allMessages) {
-        for (const reaction of reactionSelect) {
-            if (reaction.getAttribute('aria-label').includes("thisguy")) {
-                const reactionCount = selectedMessage.querySelector('.reactions-3ryImn');
-                reaction.closest('.reaction-3vwAF2').style.display = "none";
-                if (reactionCount !== null) {
-                    if (reactionCount.childElementCount === 2 && reactionCount.querySelector('[alt*="thisguy"]')) {
-                        reactionCount.style.display = "none";
-                        selectedMessage.querySelector('.container-2sjPya').style.padding = "0px";
-                    } else {
-                        reactionCount.style.display = "flex";
-                        selectedMessage.querySelector('.container-2sjPya').style.paddingBottom = "4px";
+    const root = document.getElementById('app-mount');
+    const allMessages = root.querySelectorAll(".messageListItem-ZZ7v6g");
+    const reactionSelect = root.querySelectorAll(".reactionInner-YJjOtT");
+    for (const blackList of blackLisedEmotes) {
+        for (const selectedMessage of allMessages) {
+            for (const reaction of reactionSelect) {
+                if (reaction.getAttribute('aria-label').includes(blackList)) {
+                    const reactionCount = selectedMessage.querySelector('.reactions-3ryImn');
+                    reaction.closest('.reaction-3vwAF2').style.display = "none";
+                    if (reactionCount !== null) {
+                        if (reactionCount.childElementCount === 2 && reactionCount.querySelector(`[alt*=${CSS.escape(blackList)}]`)) {
+                            reactionCount.style.display = "none";
+                            selectedMessage.querySelector('.container-2sjPya').style.padding = "0px";
+                        } else {
+                            reactionCount.style.display = "flex";
+                            selectedMessage.querySelector('.container-2sjPya').style.paddingBottom = "4px";
+                        }
                     }
                 }
             }
-        }
 
-        if (selectedMessage.querySelector('[src*="size=96&"][alt*="thisguy"]')) {
-            selectedMessage.style.display = 'none';
-            if (!selectedMessage.getAttribute('style').includes('list-item')) {
+            if (selectedMessage.querySelector(`[src*="size=96&"][alt*=${CSS.escape(blackList)}]`)) {
                 selectedMessage.style.display = 'none';
-            }
-            if (selectedMessage.getAttribute('style')) {
-                const userID = selectedMessage.querySelector('div');
-                const checkNextID = selectedMessage.nextElementSibling.querySelector('div');
-                if (userID !== null && checkNextID !== null) {
-                    if (userID.getAttribute('data-author-id') === checkNextID.getAttribute('data-author-id') && userID.getAttribute('class').includes('groupStart')) {
-                        selectedMessage.style.display = 'list-item';
+                if (!selectedMessage.getAttribute('style').includes('list-item')) {
+                    selectedMessage.style.display = 'none';
+                }
+                if (selectedMessage.getAttribute('style')) {
+                    const userID = selectedMessage.querySelector('div');
+                    const checkNextID = selectedMessage.nextElementSibling.querySelector('div');
+                    if (userID !== null && checkNextID !== null) {
+                        if (userID.getAttribute('data-author-id') === checkNextID.getAttribute('data-author-id') && userID.getAttribute('class').includes('groupStart')) {
+                            selectedMessage.style.display = 'list-item';
+                        }
                     }
                 }
             }
         }
-
     }
 }
 
-module.exports = class ExamplePlugin {
+module.exports = class Plugin {
     start() {
+        BdApi.injectCSS("Remove-Emotes", `
+        [style$="list-item;"] div div .markup-eYLPri.messageContent-2t3eCI{
+            display: none;
+        }
+
+        li[style$="list-item;"] > div {
+            min-height: 0 !important;
+            padding-bottom: 0!important;
+        }`);
+
+        const removeEmoteStyle = document.getElementById('Remove-Emotes');
+        for (const dissappearingEmote of blackLisedEmotes) {
+            let style = `[alt*=${CSS.escape(dissappearingEmote)}]{display: none;}`;
+            removeEmoteStyle.appendChild(document.createTextNode(style));
+        }
     }
 
     observer(chatBox) {
@@ -56,5 +73,6 @@ module.exports = class ExamplePlugin {
     }
 
     stop() {
+        BdApi.clearCSS("Remove-Emotes");
     }
 }
