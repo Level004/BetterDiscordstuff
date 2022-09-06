c1dfe761031b745f642bb8ea4ce5dec2d9c8f6ab
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2022-09-06 19:38:52 +0200

    started to add json for storage

--- plugins/OntopMemberLIst.plugin.js
+++ plugins/OntopMemberLIst.plugin.js
@@ -2,42 +2,30 @@
  * @name Put me ontop
  * @author Level 4
  * @description put me ontop the members list
- * @version 1.0.1
+ * @version 1.0.2
  */
 
 const root = document.getElementById('app-mount');
-
-function createLabel(){
-    const membersList = root.querySelector('.content-2a4AW9[role="list"]');
-    const h2 = document.createElement('h2');
-    const span =document.createElement('span');
-    h2.classList.add("membersGroup-2eiWxl","container-q97qHp", "ontop");
-    span.setAttribute('aria-hidden', "true");
-    span.textContent = "Guilliano — 1";
-    membersList.append(h2);
-    h2.append(span);
-}
+const membersList = root.querySelector('.content-2a4AW9[role="list"]');
 
 function putOntop() {
-    const members = root.querySelectorAll(".member-2gU6Ar.container-1oeRFJ.clickable-28SzVr");
-    for (const member of members) {
-        const name = member.querySelector('span.username-i5-wv-').firstChild.textContent;
-        if (name.includes('Guilliano')){
-            createLabel();
-            member.classList.add('ontop');
-        }
-    }
+	const members = root.querySelectorAll(".member-2gU6Ar.container-1oeRFJ.clickable-28SzVr");
+	for (const member of members) {
+		const name = member.querySelector('span.username-i5-wv-').firstChild.textContent;
+		if (name.includes('Guilliano')) {
+			member.classList.add('ontop');
+		}
+	}
 }
 
 module.exports = class Plugin {
-    start() {
-        putOntop();
-    }
+	start() {putOntop();}
 
-    onSwitch() {
-        putOntop();
-    }
+	observer(membersList) {
+		if (root.querySelector(".ontop") === null) {
+			putOntop();
+		}
+	}
 
-    stop() {
-    }
+	stop() {}
 }
